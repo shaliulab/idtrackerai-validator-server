18e110ed90fce4016ad5200f605037c8d14692c1
antortjim <ntoniohu@gmail.com>
2024-01-24 15:24:35 +0000

    Update start server

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-~/mambaforge/envs/rapids-23.04/bin/python /home/vibflysleep/opt/idtrackerai-validator-server/idtrackerai_validator_server/main.py
+/home/vibflysleep/mambaforge/envs/rapids-23.04/bin/python /home/vibflysleep/opt/idtrackerai-validator-server/idtrackerai_validator_server/main.py
