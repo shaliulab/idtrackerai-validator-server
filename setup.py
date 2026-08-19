6d6c584e0b20874aa44d43446f4ca7d48840b602
antortjim <ntoniohu@gmail.com>
2026-08-19 20:54:53 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='idtrackerai_validator_server',
-    version='1.0',
+    version='1.0.1',
     author='Antonio Ortega',
     author_email='antonio.ortega@kuleuven.be',
     description='An easy to use viewer of flyhostel data',
