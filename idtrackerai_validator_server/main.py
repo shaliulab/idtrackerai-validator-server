f119134b1e2f16296cb6b00b788c7bc5e6d1c644
antortjim <ntoniohu@gmail.com>
2026-08-19 19:51:32 +0200

    Print command to start backend

--- idtrackerai_validator_server/main.py
+++ idtrackerai_validator_server/main.py
@@ -9,12 +9,12 @@ def main():
     here=pathlib.Path(__file__).parent.absolute()
     executable=os.path.join(here, "app.py")
 
-
-    cmd=shlex.split(
-        f"""
-        {python_bin} {executable} -m flask run --host \\"0.0.0.0\\" --port {os.environ.get('BACKEND_PORT', 5000)}
-        """
-    )
+    cmd=f"""
+        {python_bin} {executable}  --host "{os.environ.get('CVAT_HOST', 3000)}" --port {os.environ.get('BACKEND_PORT', 5000)}
+    """
+    print(cmd)
+ 
+    cmd=shlex.split(cmd)
 
     p=subprocess.Popen(cmd)
     p.communicate()
