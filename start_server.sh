6b068b168af9ea0b20aa8241912e372363fa229f
antortjim <ntoniohu@gmail.com>
2026-08-26 14:44:26 +0000

    Clean

--- start_server.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-/home/vibflysleep/mambaforge/envs/validator/bin/python /home/vibflysleep/opt/idtrackerai-validator-server/idtrackerai_validator_server/main.py
--- start_server_5001.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-BACKEND_PORT=5001 /home/vibflysleep/mambaforge/envs/validator/bin/python /home/vibflysleep/opt/idtrackerai-validator-server/idtrackerai_validator_server/main.py 
